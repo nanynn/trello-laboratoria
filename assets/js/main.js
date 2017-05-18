9baaaecdedcf37c9dde94863f637f4b29169d0e5
Anayn Pavez <nanyn.pavez@gmail.com>
2017-05-18 00:49:39 -0300

    primera parte funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-alert("Hola mundo");
+function aparecer(){
+	var contenido= document.getElementById('contenido');
+	
+	var boton = document.createElement('button');
+	var espacio = document.createElement('input');
+	document.createElement('input').value = "";
+	var respuesta = document.createElement('div');
+
+
+	espacio.placeholder +='Añadir una lista...'
+	boton.innerHTML = 'Guardar';
+
+	contenido.appendChild(espacio);
+	contenido.appendChild(boton);
+	
+} 
